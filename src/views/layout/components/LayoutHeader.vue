9f98c526b65988c23d1c7cb6ae3a21cf208101ca
liang <lwy.work@outlook.com>
2024-05-31 21:37:51 +0800

    完成导航栏高亮显示

--- src/views/layout/components/LayoutHeader.vue
+++ src/views/layout/components/LayoutHeader.vue
@@ -20,7 +20,9 @@ const layoutStore = useLayoutStore()
           <RouterLink to="/">首页</RouterLink>
         </li>
         <li v-for="item in layoutStore.categoryList" :key="item.id">
-          <RouterLink :to="`/category/${item.id}`">{{ item.name }}</RouterLink>
+          <RouterLink active-class="active" :to="`/category/${item.id}`">{{
+            item.name
+          }}</RouterLink>
         </li>
       </ul>
       <div class="search">
