5d7cf03d08efc0b31e9c6e9b2e5824a57a429125
liang <lwy.work@outlook.com>
2024-05-31 20:48:04 +0800

    完成category一级路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -13,7 +13,7 @@ const router = createRouter({
           component: () => import('@/views/home/index.vue')
         },
         {
-          path: '/category',
+          path: '/category/:id',
           component: () => import('@/views/category/index.vue')
         }
       ]
--- src/views/layout/components/LayoutHeader.vue
+++ src/views/layout/components/LayoutHeader.vue
@@ -20,7 +20,7 @@ const layoutStore = useLayoutStore()
           <RouterLink to="/">首页</RouterLink>
         </li>
         <li v-for="item in layoutStore.categoryList" :key="item.id">
-          <RouterLink to="/">{{ item.name }}</RouterLink>
+          <RouterLink :to="`/category/${item.id}`">{{ item.name }}</RouterLink>
         </li>
       </ul>
       <div class="search">
